842100861d0336ec523ea38328ab1bdfca6b2300
warlockja <warlockja@gmail.com>
2023-09-06 14:37:06 +0300

    revalidation interval/on-demand tag/path

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Posts from "./components/Posts";
 
+// revalidation interval config
+// export const revalidate = 10; // seconds. normally would be a very high number 86400(day)
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
